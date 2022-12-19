9096038961105206259dfd990174532cd0e242bd
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2022-12-19 13:01:03 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -10,3 +10,33 @@ window.onscroll = () =>{
   menu.classList.remove('fa-times');
   navbar.classList.remove('active');
 }
+document.addEventListener("contextmenu", function(e) {
+    e.preventDefault();
+
+    alert("Not Allowed!!");
+   }, false);
+
+window.alert = (text = "", theme = "danger") => {
+  const getElems = () => document.querySelectorAll(".custom_alert");
+  if (getElems().length)
+    Array.from(getElems()).map((e, i) => {
+      try {
+        i === 1 ? (e.innerHTML = `<div class="alert alert-${theme}" role="alert">${text}</div>`) : e.remove();
+        clearTimeout(window.alertTimeout_1);
+        clearTimeout(window.alertTimeout_2);
+      } catch {}
+    });
+
+  document.body.insertAdjacentHTML("afterbegin", `<div class="custom_alert"><div class="alert alert-${theme}" role="alert">${text}</div></div>`);
+
+  window.alertTimeout_1 = setTimeout(() => {
+    const k = document.querySelector(".custom_alert");
+    if (k) {
+      k.classList.add("closed");
+      window.alertTimeout_2 = setTimeout(() => {
+        k.remove();
+      }, 500);
+    }
+  }, 2000);
+};
+
