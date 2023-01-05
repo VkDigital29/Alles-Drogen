72e229247dab1c8db9ec7514672c10cb43146628
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2023-01-05 21:38:24 +0530

    Update search.js

--- script/search.js
+++ script/search.js
@@ -11,7 +11,7 @@ function myFunction() {
     a = li[i].getElementsByTagName("a")[0];
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "none";
+      li[i].style.display = "";
     } else {
       li[i].style.display = "none";
     }
