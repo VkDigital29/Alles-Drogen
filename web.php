9ab0fb9d12c7c7f1308556b4c072310e15a83ecd
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2023-01-05 19:00:31 +0530

    Update web.php

--- web.php
+++ web.php
@@ -1,23 +1,3 @@
-<?php 
-  
- use Statamic\Facades\Entry; 
-  
- Route::statamic('search-results', 'search', ['hide_sidebar' => true]); 
- Route::statamic('sitemap.xml', 'sitemap', ['content_type' => 'xml', 'layout' => 'sitemap']); 
-  
- // Redirects 
-  
- Route::permanentRedirect('collections-and-entries', 'collections'); 
- Route::permanentRedirect('template-engines', 'blade-templates'); 
- Route::permanentRedirect('entries', 'collections#entries'); 
- Route::permanentRedirect('git-integration', 'git-automation'); 
- Route::permanentRedirect('installation', 'installing'); 
- Route::permanentRedirect('blade-templates', 'blade'); 
- Route::permanentRedirect('fieldtypes/partial', '/blueprints#importing-fieldsets'); 
- Route::permanentRedirect('cascade', 'data-inheritance'); 
- Route::permanentRedirect('content-api', 'rest-api'); 
- Route::permanentRedirect('using-front-end-frameworks', 'javascript-frameworks'); 
- Route::permanentRedirect('/content-queries/{slug}', '/repositories/{slug}'); 
- Route::permanentRedirect('repositories', 'content-queries'); 
- // Route::permanentRedirect('extending/queries', 'content-queries');
- />
+<?php
+Route::statamic('search, search');
+/>
