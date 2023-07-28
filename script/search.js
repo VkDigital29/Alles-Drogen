c2be52f66321e00db62ad53133b6b78c8ee114af
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2023-07-28 19:44:51 +0530

    Create search.js

---
+++ script/search.js
@@ -0,0 +1,50 @@
+/*
+This is an example snippet - you should consider tailoring it
+to your service.
+*/
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  const result = await fetch(
+    "undefined",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName
+      })
+    }
+  );
+
+  return await result.json();
+}
+
+const operationsDoc = `
+  query Acetaminophen($distinct_on: [Search_select_column!] = Query, $where: Search_bool_exp = {}) {
+    Search(distinct_on: $distinct_on, where: $where) {
+      Query
+    }
+  }
+`;
+
+function fetchAcetaminophen(distinct_on, where) {
+  return fetchGraphQL(
+    operationsDoc,
+    "Acetaminophen",
+    {"distinct_on": distinct_on, "where": where}
+  );
+}
+
+async function startFetchAcetaminophen(distinct_on, where) {
+  const { errors, data } = await fetchAcetaminophen(distinct_on, where);
+
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
+  }
+
+  // do something great with this precious data
+  console.log(data);
+}
+
+startFetchAcetaminophen(distinct_on, where);
