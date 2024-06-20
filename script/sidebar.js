07ccf7050f2c5d7f82264fbb929c57ef61523938
VkDigital29 <119841251+VkDigital29@users.noreply.github.com>
2024-06-20 20:09:03 +0530

    Delete script/sidebar.js

--- script/sidebar.js
+++
@@ -1,9 +0,0 @@
- const body = document.querySelector('body'),
-                  sidebar = body.querySelector('nav1'),
-                  toggle = body.querySelector(".toggle"),
-                  searchBtn = body.querySelector(".search-box"),
-                  modeSwitch = body.querySelector(".toggle-switch"),
-                  modeText = body.querySelector(".mode-text");
-            toggle.addEventListener("click" , () =>{
-                sidebar.classList.toggle("close");
-            })
